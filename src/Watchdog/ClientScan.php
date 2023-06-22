1c0603ed441d5d35fd804b40303b51932203633c
amalkorbi96 <amalkorbi96@gmail.com>
2023-06-22 09:38:45 +0100

    Update ClientScan.php

--- src/Watchdog/ClientScan.php
+++ src/Watchdog/ClientScan.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Smartedutech\Securelayer\Watchdog;
-
+use Smartedutech\Securelayer\Log\DBAgentLog;
 abstract class ClientScan{ 
     public static function getClient(){ 
         return json_encode(array(
             "REMOTE_ADDR"=>$_SERVER['REMOTE_ADDR'] 
             ,"HTTP_USER_AGENT"=>$_SERVER['HTTP_USER_AGENT'] 
             ,"HTTP_ACCEPT_LANGUAGE"=>$_SERVER['HTTP_ACCEPT_LANGUAGE'] 
-            ,"HTTP_AUTHORIZATION"=>$_SERVER['HTTP_AUTHORIZATION'] 
+            ,"HTTP_AUTHORIZATION"=>isset($_SERVER['HTTP_AUTHORIZATION']) 
+            
         ));  
+    }
+    public static function saveClientInfo() {
+        $clientInfo = self::getClient();
+        DBAgentLog::DBSaveLogMessage($clientInfo);
     }
 }
