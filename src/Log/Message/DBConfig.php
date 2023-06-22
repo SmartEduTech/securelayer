11babaaa92a618ec009b7af59c9bf35f50356571
amalkorbi96 <amalkorbi96@gmail.com>
2023-06-22 09:38:31 +0100

    Create DBConfig.php

---
+++ src/Log/Message/DBConfig.php
@@ -0,0 +1,10 @@
+<?php
+namespace Smartedutech\Securelayer\Log;
+
+abstract class DBConfig
+{
+    public static $host = 'localhost';
+    public static $dbName = 'my_database';
+    public static $user = 'my_user';
+    public static $password = 'my_password';
+}
