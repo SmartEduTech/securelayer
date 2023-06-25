54bb99174aa2b7921be80ba16855fecdcb075822
amalkorbi96 <amalkorbi96@gmail.com>
2023-06-25 17:38:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
 include "./vendor/autoload.php";
 
-use Smartedutech\Securelayer\Log\MessageLog;
-use Smartedutech\Securelayer\Log\AgentLog;
-use Smartedutech\Securelayer\Log\DBAgentLog;
-use Smartedutech\Securelayer\Filters\FilterDatas;
-use Smartedutech\Securelayer\Analyser\StrategyAnalyser;
 use Smartedutech\Securelayer\Watchdog\WatchDog;
-use Smartedutech\Securelayer\Watchdog\ClientScan;
-use Smartedutech\Securelayer\Watchdog\ActivityScan;
-
 
 $watchdog = new WatchDog();
-$watchdog->run('high_security_strategy');
+$watchdog->run('high_secaurity_strategy');
 
-
-$app=new StrategyAnalyser();
-$app->run();
