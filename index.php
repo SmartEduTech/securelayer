a0c69ee7a0ac4833308fd8c9cfb450edda99e0f6
amalkorbi96 <amalkorbi96@gmail.com>
2023-06-26 22:07:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,28 @@
 include "./vendor/autoload.php";
 
 use Smartedutech\Securelayer\Watchdog\WatchDog;
+use Smartedutech\Securelayer\Analyser\StrategyAnalyser;
+//$main = new StrategyAnalyser();
+//$watchdog = new WatchDog();
+//$watchdog->run('high_secaurity_strategy');
 
-$watchdog = new WatchDog();
-$watchdog->run('high_secaurity_strategy');
+
+
+
+if(count($_POST)>0){
+echo "test";
+}else{
+    ?>
+<form method="post">
+    <input type="text" name="email">
+    <input type="submit" value="send">
+</form>
+    <?php
+}
+
+
+
+$app=new StrategyAnalyser();
+$app->run();
+
 
